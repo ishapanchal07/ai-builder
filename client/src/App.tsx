fee614796700b15b4feaf42d2f209f5cbe2651f1
ishapanchal07 <isha7112003@gmail.com>
2025-12-18 16:19:19 +0530

    route error fixed in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Pricing from './pages/Pricing'
@@ -14,26 +13,28 @@ import Settings from './pages/Setting'
 
 const App = () => {
   const { pathname } = useLocation()
-  const hideNavbar = pathname.startsWith('/projects/') && pathname !== '/projects' 
-  || pathname.startsWith('/view/')
-  || pathname.startsWith('/preview/')
+
+  const hideNavbar =
+    (pathname.startsWith('/projects/') && pathname !== '/projects') ||
+    pathname.startsWith('/view/') ||
+    pathname.startsWith('/preview/')
 
   return (
     <div>
       <Toaster />
       {!hideNavbar && <Navbar />}
-      
+
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/pricing' element={<Pricing />} />
-        <Route path='/projects/:projectId' element={<Projects />} />
-        <Route path='/projects' element={<MyProjects />} />
-        <Route path='/preview/:projectId' element={<Preview />} />
-        <Route path='/preview/:projectId/:versionId' element={<Preview />} />
-        <Route path='/community' element={<Community />} />
-        <Route path='/view/:projectId' element={<View />} />
-        <Route path="/auth/:pathname" element={<AuthPage />} />
-        <Route path="/account/:settings" element={<Settings />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/projects" element={<MyProjects />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/preview/:projectId" element={<Preview />} />
+        <Route path="/preview/:projectId/:versionId" element={<Preview />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/view/:projectId" element={<View />} />
+        <Route path="/auth/*" element={<AuthPage />} />
+        <Route path="/account/*" element={<Settings />} />
       </Routes>
     </div>
   )
